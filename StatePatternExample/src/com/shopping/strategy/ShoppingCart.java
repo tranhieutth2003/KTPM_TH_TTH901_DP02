c3abd80d6e8fe8d640d641eba656871251d7f850
H82M19\Student <Student>
2025-03-11 19:44:47 +0700

    ShoppingCart

---
+++ StatePatternExample/src/com/shopping/strategy/ShoppingCart.java
@@ -0,0 +1,34 @@
+package com.shopping.strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    // Danh sách sản phẩm
+    private List<Item> items;
+
+    public ShoppingCart() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.items.remove(item);
+    }
+
+    public int calculateTotal() {
+        int sum = 0;
+        for (Item item : items) {
+            sum += item.getPrice();
+        }
+        return sum;
+    }
+
+    public void pay(PaymentStrategy paymentMethod) {
+        int amount = calculateTotal();
+        paymentMethod.pay(amount);
+    }
+}
