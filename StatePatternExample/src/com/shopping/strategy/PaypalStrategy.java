2f1204b3e5599163a2b28372789325d0634726b5
H82M19\Student <Student>
2025-03-11 19:43:31 +0700

    PaypalStrategy

---
+++ StatePatternExample/src/com/shopping/strategy/PaypalStrategy.java
@@ -0,0 +1,16 @@
+package com.shopping.strategy;
+
+public class PaypalStrategy implements PaymentStrategy {
+    private String emailId;
+    private String password;
+
+    public PaypalStrategy(String email, String password) {
+        this.emailId = email;
+        this.password = password;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println(amount + " paid using PayPal.");
+    }
+}
